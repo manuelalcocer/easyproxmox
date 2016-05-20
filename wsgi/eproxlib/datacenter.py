587bc3d1ce013874b51830cdc673a357f9329df9
manuelalcocer <m.alcocer1978@gmail.com>
2016-05-20 19:35:39 +0200

    nuevo

--- wsgi/eproxlib/datacenter.py
+++ wsgi/eproxlib/datacenter.py
@@ -12,7 +12,7 @@ class DataCenter:
     def FetchCreds(self):
         parameters_list = { 'username' : self.creds['username'] + '@pam', 'password' : self.creds['password'] }
         self.api_root = self.https_url + self.api_address
-        #self.creds_response = requests.post(self.api_root + self.api_ticket, params = parameters_list)
+        self.creds_response = requests.post(self.api_root + self.api_ticket, params = parameters_list)
         #self.json_creds = json.loads(self.creds_response.text)
         #self.creds['cookie'] = { 'PVEAuthCookie' : self.json_creds['data']['ticket'] }
         #self.creds['header'] = { 'CSRFPreventionToken' : self.json_creds['data']['CSRFPreventionToken'] }
