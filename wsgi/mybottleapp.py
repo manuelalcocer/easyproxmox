7dbe33ded3048312bd83e78a8ef7dd5ffdac4de3
manuelalcocer <m.alcocer1978@gmail.com>
2016-05-20 19:43:49 +0200

    nuevo

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -2,7 +2,28 @@ from bottle import route, default_app, template, static_file, request
 import json
 import requests
 
-from eproxlib.datacenter import DataCenter as MyDataCenter
+#from eproxlib.datacenter import DataCenter as MyDataCenter
+
+class DataCenter:
+    def __init__(self, id_name):
+        self.id_name = id_name
+        self.https_url = ''
+        self.api_address = ''
+        self.api_ticket = ''
+        self.creds = {}
+
+    def FetchCreds(self):
+        parameters_list = { 'username' : self.creds['username'] + '@pam', 'password' : self.creds['password'] }
+        self.api_root = self.https_url + self.api_address
+        self.creds_response = requests.post(self.api_root + self.api_ticket, params = parameters_list)
+        #self.json_creds = json.loads(self.creds_response.text)
+        #self.creds['cookie'] = { 'PVEAuthCookie' : self.json_creds['data']['ticket'] }
+        #self.creds['header'] = { 'CSRFPreventionToken' : self.json_creds['data']['CSRFPreventionToken'] }
+        #self.cookie = dict(PVEAuthCookie=self.json_creds['data']['ticket'])
+
+    def FetchNodeList(self):
+        self.NodePath = self.api_root + '/nodes'
+        self.json_nodelist = json.loads(requests.get(self.NodePath, cookies = self.creds['cookie']).text)
 
 @route('/')
 def index():
@@ -10,7 +31,7 @@ def index():
 
 @route('/fetchtoken', method='POST')
 def CogerToken():
-    proxhome = MyDataCenter('nashgul')
+    proxhome = DataCenter('nashgul')
     proxhome.https_url = 'https://proxmox.nashgul.com.es'
     proxhome.api_address = '/api2/json'
     proxhome.api_ticket = '/access/ticket'
