f38151d9bdd430e9d9d4821b027fc5133cbf5f2b
manuelalcocer <m.alcocer1978@gmail.com>
2016-05-23 19:27:59 +0200

    beaker

--- wsgi/eproxlib/proxdatabase.py
+++ wsgi/eproxlib/proxdatabase.py
@@ -1,36 +1,36 @@
-    def CreateConn(dbname, dbuser, dbhost, dbpassword):
-        try:
-            conn = psycopg2.connect("dbname=%s user=%s host=%s password=%s" %(dbname, dbuser, dbhost, dbpassword))
-            cur = conn.cursor()
-            return conn, cur
-        except:
-            pass
+def CreateConn(dbname, dbuser, dbhost, dbpassword):
+    try:
+        conn = psycopg2.connect("dbname=%s user=%s host=%s password=%s" %(dbname, dbuser, dbhost, dbpassword))
+        cur = conn.cursor()
+        return conn, cur
+    except:
+        pass
 
-    def CloseConn(conn, cur):
-        cur.close()
-        conn.close()
+def CloseConn(conn, cur):
+    cur.close()
+    conn.close()
 
-    def DataCenterList(conn, cur):
-        cur.execute("""SELECT * from centros_de_datos;""")
-        datacenterlist = cur.fetchall()
-        return datacenterlist
+def DataCenterList(conn, cur):
+    cur.execute("""SELECT * from centros_de_datos;""")
+    datacenterlist = cur.fetchall()
+    return datacenterlist
 
-    def InsertDataCenter(**kwargs):
-        centername = kwargs['centername']
-        url = kwargs['url']
-        port = kwargs['port']
-        cur.execute("""INSERT INTO centros_de_datos (nombre, url, puerto) values (%s, %s, %s)""", (centername, url, port))
-        conn.commit()
-        CloseConn(conn, cur)
+def InsertDataCenter(**kwargs):
+    centername = kwargs['centername']
+    url = kwargs['url']
+    port = kwargs['port']
+    cur.execute("""INSERT INTO centros_de_datos (nombre, url, puerto) values (%s, %s, %s)""", (centername, url, port))
+    conn.commit()
+    CloseConn(conn, cur)
 
-    def InsertUser(self, **kwargs):
-        username = kwargs['username']
-        centername = kwargs['centername']
-        cur.execute("""INSERT INTO usuarios (nombre, centro) values (%s, %s)""", (username, centername))
-        conn.commit()
+def InsertUser(self, **kwargs):
+    username = kwargs['username']
+    centername = kwargs['centername']
+    cur.execute("""INSERT INTO usuarios (nombre, centro) values (%s, %s)""", (username, centername))
+    conn.commit()
 
-    def InfoCenter(**kwargs):
-        centername = kwargs['centername']
-        cur.execute("""select * from centros_de_datos where nombre = %s;""", centername)
-        infocenter = cur.fetchone()
-        return infocenter
+def InfoCenter(**kwargs):
+    centername = kwargs['centername']
+    cur.execute("""select * from centros_de_datos where nombre = %s;""", centername)
+    infocenter = cur.fetchone()
+    return infocenter
